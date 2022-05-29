ee6b6ad6a96b6f09c3763c0093610af5b1dbd1c1
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-05-28 23:53:15 -0500

    se agrego correciones de estilos

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
- let campos = document.getElementsByClassName("entrada");
+let campos = document.getElementsByClassName("entrada");
 
- /*
- let valForm = document.getElementById("formulario").addEventListener("submit",()=>{
-    console.log("Script funcionando");
-    comprobarCampos();
+/*
+let valForm = document.getElementById("formulario").addEventListener("submit",()=>{
+   console.log("Script funcionando");
+   comprobarCampos();
 })
 */
 
@@ -24,19 +24,36 @@ for(var i=0; i<campos.length; i++){
 }
 
 */
+//creacion del mensaje a mostrar
+const p = document.createElement("p");
+p.id = "advertencia";
+//posicion en donde se mostrara
+const frm = document.querySelector(".nombre");
 
-const comprobarCampos = (event)=>{
+
+const comprobarCampos = (event) => {
     event.preventDefault();//previene que se recargue la pagina
-//destructuracion del elemento
-     const[name, lastName, email, password] = event.target;
-    /*
-    //manera tradicional de solicitar el valor del campo de texto
-    console.log(event.target.name.value,
-                event.target.lastName.value,
-                event.target.mail.value,
-                event.target.password.value);
-                */
-    console.log(name.value,lastName.value,email.value,password.value);
-}
+    //destructuracion del elemento
+    const [name, lastName, email, password] = event.target;
 
+    /*
+   //manera tradicional de solicitar el valor del campo de texto
+   console.log(event.target.name.value,
+               event.target.lastName.value,
+               event.target.mail.value,
+               event.target.password.value);
+   */
+    //console.log(name.value,lastName.value,email.value,password.value);
+    
+    if (name.value === "") {
+        p.textContent = "El campo no debe estar vacio";
+        frm.insertAdjacentElement("afterend", p);
+    }
+    else if (lastName.value === "") {
+        const frm = document.querySelector(".apellido");
+        p.textContent = "El campo apellido no debe estar vacio";
+        frm.insertAdjacentElement("afterend", p);
+    }
+}
 form.addEventListener("submit", comprobarCampos);
+
